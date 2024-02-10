bc8f9c7d2279f78d39ed7eceec67f335af970708
Oleksii Volkov <lifecoder.ua@gmail.com>
2024-02-10 22:48:20 +0200

    apollo with type issues

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,24 @@
 <script setup lang="ts">
 import { RouterLink, RouterView } from 'vue-router'
+import { useQuery } from '@vue/apollo-composable'
+import gql from 'graphql-tag'
 import HelloWorld from './components/HelloWorld.vue'
+
+const { result } = useQuery(gql`
+      query getUsers {
+        users {
+          id
+          firstname
+          lastname
+          email
+        }
+      }
+    `)
 </script>
 
 <template>
   <header>
+    {{ result }}
     <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
 
     <div class="wrapper">
